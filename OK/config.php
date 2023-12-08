5e4d2fb8d112d8c601adef19a3a8a14d0dc81617
Eduardo Rosales <eduardorosales720@gmail.com>
2023-12-08 10:42:40 -0600

    First Load

---
+++ OK/config.php
@@ -0,0 +1,29 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.deepsoundscript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | DeepSound - The Ultimate PHP Sound Sharing Platform
+// | Copyright (c) 2019 DeepSound. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "music_evo";
+// MySQL Database Password
+$sql_db_pass = "@mexi2019";
+// MySQL Database Name
+$sql_db_name = "music_evo";
+
+// Site URL
+$site_url = "http://musicevolution.network"; // e.g (http://example.com)
+
+$developer_mode = 0;
+
+$auto_redirect = true;
+
+// Purchase code
+$purchase_code = "MGI"; // Your purchase code, don't give it to anyone.
+?>
